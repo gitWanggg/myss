0458ceb15d3a8198e27b4d702fa073fb9b93d7ad
gitWanggg <wsp_1989@126.com>
2019-09-13 16:21:52 +0800

    002

---
+++ SS.Service/SSException.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SS.Service
+{
+    public class SSException:Exception
+    {
+         // 摘要: 
+        //     初始化 System.Exception 类的新实例。
+        public SSException()
+        {
+
+        }
+        //
+        // 摘要: 
+        //     使用指定的错误信息初始化 System.Exception 类的新实例。
+        //
+        // 参数: 
+        //   message:
+        //     描述错误的消息。
+        public SSException(string message)
+            :base(message)
+        {
+
+        }
+        
+    }
+}
