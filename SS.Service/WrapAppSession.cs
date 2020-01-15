b88ce7d29d21fde82f7ec713ec498012210d3d62
gitWanggg <wsp_1989@126.com>
2020-01-15 21:00:22 +0800

    002

--- SS.Service/WrapAppSession.cs
+++ SS.Service/WrapAppSession.cs
@@ -27,7 +27,18 @@ namespace SS.Service
         //{
 
         //}
-
+        public override void Send(ArraySegment<byte> segment)
+        {
+            base.Send(segment);
+        }
+        public override void Send(byte[] data, int offset, int length)
+        {
+            string outString = "";
+            for (int i = 0; i < length;i++ )                
+                    outString += data[offset+i].ToString("X2");
+            this.Logger.Debug("回复:" + outString);
+            base.Send(data, offset, length);
+        }
         public int LoggerLevel { get; set; }
         /// <summary>
         /// 消息流水号
